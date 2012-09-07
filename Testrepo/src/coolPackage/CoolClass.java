8c05913ad68c015d30d2b5475200f88b007538f3
Kryel <mc@kryel.com>
2012-09-07 21:26:35 +0200

    Bedre nå?

--- Testrepo/src/coolPackage/CoolClass.java
+++ Testrepo/src/coolPackage/CoolClass.java
@@ -4,6 +4,10 @@ public class CoolClass {
 	private static String coolString = "This String is Cool";
 
 	public static void main(String[] args) {
+		new CoolClass();
+	}
+
+	public CoolClass() {
 		coolMethod();
 	}
 
