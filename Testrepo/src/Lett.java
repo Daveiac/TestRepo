18fac0779ca8df8134b0fd12c8f355bccf1da2a5
Kryel <mc@kryel.com>
2012-09-07 21:13:02 +0200

    Committing - again...

--- Testrepo/src/Lett.java
+++ Testrepo/src/Lett.java
@@ -4,7 +4,11 @@ public class Lett {
 
 	public static void main(String[] args) {
 		System.out.println("Dette var ikke SÅ lett!");
-
+		for (int i = 0; i < 43; i++) {
+			if (i > 41) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
--- Testrepo/src/NyKlasse.java
+++ Testrepo/src/NyKlasse.java
@@ -1,6 +1,9 @@
 
 public class NyKlasse {
-	
+	public static void main(String[] args) {
+		System.out.println("Klassen er ny.");
+		
+	}
 	//Denne klassen er ny
 
 }
