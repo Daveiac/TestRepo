1c062b28dbd13dae57c1d144361a798180c34483
Kryel <mc@kryel.com>
2012-09-07 21:18:55 +0200

    CoolCommit

---
+++ Testrepo/src/coolPackage/CoolClass.java
@@ -0,0 +1,10 @@
+package coolPackage;
+
+public class CoolClass {
+	private static String coolString = "This String is Cool";
+
+	public static void main(String[] args) {
+		System.out.println(coolString);
+	}
+
+}
