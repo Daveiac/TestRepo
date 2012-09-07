8e4e800a391831ff4c7a5328e52c83b8d1300a3b
Kryel <mc@kryel.com>
2012-09-07 22:52:47 +0200

    Will this work?

--- AwesomeRepo/src/awesomePackage/AwesomeClass.java
+++ AwesomeRepo/src/awesomePackage/AwesomeClass.java
@@ -3,5 +3,6 @@ package awesomePackage;
 public class AwesomeClass {
 	public static void main (String[] args) {
 		System.out.println("You should get the idea by now");
+		System.out.println("I think");
 	}
 }
