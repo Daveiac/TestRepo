cffdaaf3d4b451d0590e9a52ae5ef726eedbab3f
Kryel <mc@kryel.com>
2012-09-08 01:01:41 +0200

    Mystery

--- GammeltProsjekt/src/OldClass.java
+++
@@ -1,9 +0,0 @@
-
-public class OldClass {
-	
-	public static void main(String[] args) {
-		System.out.println("Old class is old");
-	}
-	
-
-}
---
+++ GammeltProsjekt/src/main/Mystery.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Mystery {
+	
+	public static void main(String[] args) {
+		System.out.println("Mysteryclass is mysterious");
+	}
+	
+
+}
