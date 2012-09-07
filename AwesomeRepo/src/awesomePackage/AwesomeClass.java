cd6408bc0b2f6426ee564fdeea0828db59e9974a
David <david.hovind@gmail.com>
2012-09-07 22:59:43 +0200

    It worked

--- AwesomeRepo/src/awesomePackage/AwesomeClass.java
+++ AwesomeRepo/src/awesomePackage/AwesomeClass.java
@@ -4,5 +4,6 @@ public class AwesomeClass {
 	public static void main (String[] args) {
 		System.out.println("You should get the idea by now");
 		System.out.println("I think");
+		System.out.println("Yes, yes it worked!");
 	}
 }
