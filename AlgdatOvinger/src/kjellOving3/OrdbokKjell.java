09996a93d578008400c7f0aa5edc5f6d3af16f97
Kryel <mc@kryel.com>
2012-09-12 17:29:23 +0200

    OrdbokKjell - Ver 1.0 Tidstester: 1622 ms

--- AlgdatOvinger/src/kjellOving3/OrdbokKjell.java
+++ AlgdatOvinger/src/kjellOving3/OrdbokKjell.java
@@ -1,4 +1,5 @@
 package kjellOving3;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,63 +10,90 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class OrdbokKjell{
+	static int pos = 0;
 
-    public static Node bygg(String[] ordliste){
-        // SKRIV DIN KODE HER
-    	return null;
-    }
+	public static Node bygg(String[] ordliste){
+		Node rot = new Node();
+		for (String ord : ordliste) {
+			createNode(rot, ord);
+		}
+		return rot;
+	}
 
-    public static ArrayList<Integer> posisjoner(String ord, int index, Node currentNode){
-        // SKRIV DIN KODE HER
-    	return null;
-    }
+	private static void createNode(Node node, String ord) {
+		if (ord.length() != 0) {
+			if (! node.barn.containsKey(ord.charAt(0))) {
+				node.barn.put(ord.charAt(0), new Node());
+			}
+			if (ord.length() == 1) {
+				node.barn.get(ord.charAt(0)).posisjoner.add(pos);
+			}
+			createNode(node.barn.get(ord.charAt(0)), ord.substring(1));
+		}
+		pos++;
+	}
+	
+	public static ArrayList<Integer> posisjoner(String ord, int index, Node currentNode){
+		if (ord.length() == 0) {
+			return (currentNode == null ? new ArrayList<Integer>() : currentNode.posisjoner);
+		} else if (ord.charAt(0) == '?') {
+			ArrayList<Integer> poss = new ArrayList<Integer>();
+			for (Node node : currentNode.barn.values()) {
+				poss.addAll(posisjoner(ord.substring(1), index + 1, node));
+			}
+			return poss;
+		} else if (currentNode == null) {
+			return new ArrayList<Integer>();
+		} else if (! currentNode.barn.containsKey(ord.charAt(0))) {
+			return new ArrayList<Integer>();
+		} else {
+			return posisjoner(ord.substring(1), index + 1, currentNode.barn.get(ord.charAt(0)));
+		}
+	}
 
-
-    public static void main(String[]  args){
-        try{
-            BufferedReader in;
-            if (args.length > 0) {
-                try {
-                    in = new BufferedReader(new FileReader(args[0]));
-                }
-                catch (FileNotFoundException e) {
-                    System.out.println("Kunne ikke åpne filen " + args[0]);
-                    return;
-                }
-            }
-            else {
-                in = new BufferedReader(new InputStreamReader(System.in));
-            }
-            StringTokenizer st = new StringTokenizer(in.readLine());
-            String[] ord = new String[st.countTokens()];
-            int i=0;
-            while(st.hasMoreTokens()) ord[i++]=st.nextToken();
-            Node rotNode = bygg(ord);
-            String sokeord= in.readLine();
-            while(sokeord!=null){
-                sokeord=sokeord.trim();
-                System.out.print(sokeord+":");
-                ArrayList<Integer> pos = posisjoner(sokeord, 0, rotNode);
-                int[] posi = new int[pos.size()];
-                for(i=0;i<posi.length;i++)posi[i]=((Integer)pos.get(i)).intValue();
-                Arrays.sort(posi);
-                for(i=0;i<posi.length;i++) System.out.print(" "+posi[i]);
-                System.out.println();
-                sokeord=in.readLine();
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[]  args){
+		try{
+			BufferedReader in;
+			if (args.length > 0) {
+				try {
+					in = new BufferedReader(new FileReader(args[0]));
+				} catch (FileNotFoundException e) {
+					System.out.println("Kunne ikke åpne filen " + args[0]);
+					return;
+				}
+			} else {
+				in = new BufferedReader(new InputStreamReader(System.in));
+			}
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			String[] ord = new String[st.countTokens()];
+			int i=0;
+			while(st.hasMoreTokens()) ord[i++]=st.nextToken();
+			Node rotNode = bygg(ord);
+			String sokeord= in.readLine();
+			while(sokeord!=null){
+				sokeord=sokeord.trim();
+				System.out.print(sokeord+":");
+				ArrayList<Integer> pos = posisjoner(sokeord, 0, rotNode);
+				int[] posi = new int[pos.size()];
+				for(i=0;i<posi.length;i++)posi[i]=((Integer)pos.get(i)).intValue();
+				Arrays.sort(posi);
+				for(i=0;i<posi.length;i++) System.out.print(" "+posi[i]);
+				System.out.println();
+				sokeord=in.readLine();
+			}
+		} catch(Exception e){
+			System.out.println();
+			e.printStackTrace();
+		}
+	}
 }
 
 class Node{
-    public ArrayList<Integer> posisjoner;
-    public HashMap<Character, Node> barn;
+	public ArrayList<Integer> posisjoner;
+	public HashMap<Character, Node> barn;
 
-    public Node(){
-        posisjoner=new ArrayList<Integer>();
-        barn=new HashMap<Character, Node>();
-    }
+	public Node(){
+		posisjoner=new ArrayList<Integer>();
+		barn=new HashMap<Character, Node>();
+	}
 }
