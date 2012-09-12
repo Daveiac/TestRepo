c791947bbb462bf7616a46055ecd037847205ddc
Kryel <mc@kryel.com>
2012-09-12 15:07:23 +0200

    Minor changes in kanskjeGaussSeidel

--- Matematikk/src/kanskjeGaussSeidel.py
+++ Matematikk/src/kanskjeGaussSeidel.py
@@ -1,10 +1,10 @@
-A = [[4,-1,0],
-     [-1,4,-1],
-     [0,-1,4]]
+A = [[4,0,0],
+     [-1,4,0],
+     [-1,-1,4]]
 
-x = [[1,1,1]]
+x = [[0,0,0]]
 
-b = [21,-45,33]
+b = [4,3,2]
 
 def GaussSeidel(itr,S): #S = signifikante siffer
     n = len(A)
@@ -17,8 +17,8 @@ def GaussSeidel(itr,S): #S = signifikante siffer
             for t in range(j+1,n):
                 x[m+1][j]-= A[j][t]*x[m][t]
             x[m+1][j] *= (1.0/A[j][j])
-            print "x["+str(m+1)+"]["+str(j)+"]",round(x[m+1][j],S-1)
-        print 
+            print("x["+str(m+1)+"]["+str(j)+"]",round(x[m+1][j],S-1))
+        print()
 
 GaussSeidel(5,6)
 
