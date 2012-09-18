bb4f1809fff2f3ac07cb14d293e0710d00adbc42
Kryel <mc@kryel.com>
2012-09-18 17:27:25 +0200

    Oppdatert div. øvinger

--- AlgdatOvinger/src/kjellOving3/OrdbokKjell.java
+++ AlgdatOvinger/src/kjellOving3/OrdbokKjell.java
@@ -1,3 +1,5 @@
+package kjellOving3;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
--- AlgdatOvinger/src/kjellOving4/Prinsessejakt.java
+++ AlgdatOvinger/src/kjellOving4/Prinsessejakt.java
@@ -3,19 +3,72 @@ package kjellOving4;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileReader;
-import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.math.BigDecimal;
 
 public class Prinsessejakt{
 
-	public static BigDecimal subgraftetthet(boolean[][] nabomatrise, int startnode){
-		int antallKanter = 1;
-		int antallNoderMed = 1;
+	static ArrayList<Node> noderMed = new ArrayList<Node>();
+	
+	public static BigDecimal subgraftetthet(boolean[][] nabomatrise, int startnode, int len){
+		Node[] noder = buildNodes(nabomatrise, len);
 		
-		//TODO: SKRIV DIN KODE HER
+		finnNoderMed(noder[startnode]);
+		int antallNoderMed = len - noderMed.size();
+		ArrayList<Integer> antallKanterFraNoder = new ArrayList<Integer>(len);
+		for (int i = 0; i < len; i++) {
+			antallKanterFraNoder.add(finnKanterIkkeMed(noder[i], i));
+		}
 		
-		return new BigDecimal(antallKanter).divide(new BigDecimal(antallNoderMed*antallNoderMed), 3, BigDecimal.ROUND_HALF_UP);
+		int antallKanter = Collections.max(antallKanterFraNoder);
+		
+		if (antallNoderMed == 0) {
+			return new BigDecimal(0).divide(new BigDecimal(1), 3, BigDecimal.ROUND_HALF_UP);
+		} else {
+			return new BigDecimal(antallKanter).divide(new BigDecimal(antallNoderMed*antallNoderMed), 3, BigDecimal.ROUND_HALF_UP);
+		}
+	}
+
+	private static Integer finnKanterIkkeMed(Node node, int start) {
+		if (noderMed.contains(node)) {
+			return 0;
+		}
+		node.visited = start;
+		int antall = 0; 
+		for (Node child : node.naboer) {
+			if (child.visited != start && !noderMed.contains(child)) {
+				antall++;
+				antall += finnKanterIkkeMed(child, start);
+			}
+		}
+		return antall;
+	}
+
+	private static void finnNoderMed(Node node) {
+		node.visited = -1;
+		noderMed.add(node);
+		for (Node child : node.naboer) {
+			if (child.visited != -1) {
+				finnNoderMed(child);
+			}
+		}
+	}
+
+	private static Node[] buildNodes(boolean[][] nabomatrise, int len) {
+		Node[] noder = new Node[len];
+		for (int i = 0; i < len; i++) {
+			noder[i] = new Node();
+		}
+		for (int i = 0; i < len; i++) {
+			boolean[] row = nabomatrise[i];
+			for (int j = 0; j < len; j++) {
+				if (row[j]) {
+					noder[i].naboer.add(noder[j]);
+				}
+			}
+		}
+		return noder;
 	}
 
 
@@ -39,11 +92,17 @@ public class Prinsessejakt{
 			String linje = in.readLine();
 			while(linje!=null && linje.length()>0){
 				int startnode= Integer.parseInt(linje);
-				System.out.println(subgraftetthet(nabomatrise, startnode));                                                        
+				System.out.println(subgraftetthet(nabomatrise, startnode, n));                                                        
 				linje=in.readLine();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+}
+
+class Node {
+	int visited = -2;
+	ArrayList<Node> naboer = new ArrayList<Node>();
+	public String toString() {return "(" + this.hashCode() + ")";}
 }
