1bbf255b070b29c0e9f93fc9874ce6ec7f347475
Kryel <mc@kryel.com>
2012-09-23 14:50:19 +0200

    Ovinger updated Made two source folders and added oving6

--- AlgdatOvinger/srcKjell/oving3/CopyOfOrdbokKjell.java
+++ AlgdatOvinger/srcKjell/oving3/CopyOfOrdbokKjell.java
@@ -1,4 +1,4 @@
-package kjellOving3;
+package oving3;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
--- AlgdatOvinger/srcKjell/oving3/OrdbokKjell.java
+++ AlgdatOvinger/srcKjell/oving3/OrdbokKjell.java
@@ -1,4 +1,4 @@
-package kjellOving3;
+package oving3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
